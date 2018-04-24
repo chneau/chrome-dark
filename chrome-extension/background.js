72f712ae046112458d875a40c9937acfe81ba66f
Charles N <charlesnowow@gmail.com>
2018-04-24 19:58:52 +0100

    deleted options.html now useless + click on icon to toggle

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,23 +1,25 @@
+let manageTab = (tab) => {
+    let host = /.*\/\/([^\/]*)\//.exec(tab.url)[1];
+    chrome.storage.sync.get(host, (data) => {
+        if (data[host] != true && data[host] != false) {
+            data[host] = true;
+        }
+        chrome.storage.sync.set({ [host]: !data[host] }, () => {
+            console.log({ [host]: !data[host] });
+            chrome.tabs.sendMessage(tab.id, !data[host]);
+        });
+    });
+}
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
-        title: `Toggle for this website`,
+        title: `Dark: Toggle for this website`,
         contexts: [`all`],
         onclick: (info, tab) => {
-            let host = /.*\/\/([^\/]*)\//igm.exec(info.pageUrl)[1];
-            chrome.storage.sync.get(host, (data) => {
-                if (data[host] != true && data[host] != false) {
-                    data[host] = true;
-                }
-                chrome.storage.sync.set({ [host]: !data[host] }, () => {
-                    console.log({ [host]: !data[host] });
-                    chrome.tabs.sendMessage(tab.id, !data[host]);
-                });
-            });
+            manageTab(tab);
         }
     });
 });
-
-
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     chrome.storage.sync.get(msg.host, (data) => {
@@ -28,4 +30,8 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
         }
     });
     return true;
+});
+
+chrome.browserAction.onClicked.addListener((tab) => {
+    manageTab(tab);
 });
--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,11 +1,9 @@
-console.log(`This is the start !`);
 let link = document.createElement(`link`);
 link.id = `darkdarkdark`;
 link.href = chrome.extension.getURL(`dark.css`);
 link.type = `text/css`;
 link.rel = `stylesheet`;
 document.getElementsByTagName(`html`)[0].appendChild(link);
-console.log(`This is the end !`);
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request) {
