642184023fad270dcdde87894dfc8e0ce8857efe
chneau <charles63500@gmail.com>
2020-02-08 22:40:33 +0000

    zibest

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -11,4 +11,43 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => link.hre
 
 chrome.runtime.sendMessage({ host: location.host }, (response) => link.href = response ? darkCSS : noCSS);
 
-setInterval(() => [...document.querySelectorAll('.tw-button--success')].forEach(x => x.click()), 1000); // for twitch
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+document.addEventListener("DOMContentLoaded", async function () {
+
+    if (!location.host.endsWith("twitch.tv")) return;
+
+    function getUsersColor() {
+        let usernames = [...document.querySelectorAll(".chat-line__username")];
+        return usernames.reduce((acc, cur) => {
+            acc[cur.innerText.toLowerCase()] = cur.style.color;
+            return acc;
+        }, {});
+    }
+
+    async function getElementByAllMeans(selector) {
+        var el = document.querySelector(selector);
+        var tries = 100;
+        while (el == null && --tries) {
+            await sleep(1);
+            el = document.querySelector(selector);
+        }
+        return el;
+    }
+
+    new MutationObserver(() => { // coloring mentions
+        let mentions = [...document.querySelectorAll('.mention-fragment:not([style]), .chat-line__message-mention:not([style])')];
+        if (mentions.length == 0) return;
+        let usersColor = getUsersColor();
+        mentions.forEach(x => {
+            let color = usersColor[x.innerText.substring(1).toLowerCase()];
+            if (color == null) return;
+            x.style = `color: ${color};`;
+        });
+    }).observe(await getElementByAllMeans(".chat-list__list-container"), { attributes: true, childList: true });
+
+    new MutationObserver(() => { // clicking the reward button
+        [...document.querySelectorAll('.tw-button--success')].forEach(x => x.click())
+    }).observe(await getElementByAllMeans(".chat-input__buttons-container"), { attributes: true, childList: true });
+
+}, false);
